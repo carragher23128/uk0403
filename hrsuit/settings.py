1435fa34ec4f11697cc4eb7265b05691834ccd03
carragher23128 <163001328+carragher23128@users.noreply.github.com>
2024-04-03 21:33:29 +0900

    Update settings.py

--- hrsuit/settings.py
+++ hrsuit/settings.py
@@ -87,8 +87,13 @@ WSGI_APPLICATION = 'hrsuit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': 'DATABASE_URL',
+        'NAME': 'PGDATABASE',
+        'USER': 'PGUSER',
+        'PASSWORD': 'PGPASSWORD',
+        'HOST': 'PGHOST',
+        'PORT': PGPORT,
     }
 }
 
