194eb08a67e4974517ea99fac8a58e4f0159432d
carragher23128 <163001328+carragher23128@users.noreply.github.com>
2024-04-14 17:35:31 +0900

    Update wsgi.py

--- hrsuit/wsgi.py
+++ hrsuit/wsgi.py
@@ -9,6 +9,11 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
+
+#追加
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
 application = get_wsgi_application()
 app = application
